ecb113a22fc52efb646c7c353d8ee1292179ae8e
Paloma lacerda <56915694+palomallacerda@users.noreply.github.com>
2021-05-28 15:28:19 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 def display_equation(user_input):
     plt.axes([0.01, 0.01, 0.01, 0.01], frameon=False)
-    plt.title('${}$'.format(user_input), fontsize=30)
+    plt.title('${}$'.format(user_input), fontsize=18)
     plt.xticks([])
     plt.yticks([])
-    plt.savefig('generated-eq.png', bbox_inches='tight', transparent=True)
+    plt.savefig('generated-eq.png', bbox_inches='tight', transparent=False)
     return 'generated-eq.png'
